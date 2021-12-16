d6f52219eee940723f0aa62998010b1330e92460
theoneee <625805189@qq.com>
2021-12-16 11:13:27 +0800

    优化启动界面代码

--- app/src/main/java/com/theone/dynamicwallpaper/ui/activity/LauncherActivity.kt
+++ app/src/main/java/com/theone/dynamicwallpaper/ui/activity/LauncherActivity.kt
@@ -1,17 +1,12 @@
 package com.theone.dynamicwallpaper.ui.activity
 
-import android.util.SparseArray
 import android.view.View
-import android.view.animation.Animation
-import com.qmuiteam.qmui.util.QMUIViewHelper
+import androidx.annotation.StringDef
 import com.theone.common.ext.startActivity
 import com.theone.dynamicwallpaper.databinding.ActivityLauncherBinding
-import com.theone.mvvm.base.activity.BaseQMUIActivity
-import com.theone.mvvm.base.activity.BaseVmDbActivity
 import com.theone.mvvm.base.viewmodel.BaseViewModel
 import com.theone.mvvm.core.base.activity.BaseCoreActivity
 import com.theone.mvvm.core.widge.pullrefresh.WWLoadingView
-import com.theone.mvvm.entity.ProgressBean
 
 //  ┏┓　　　┏┓
 //┏┛┻━━━┛┻┓
@@ -33,41 +28,48 @@ import com.theone.mvvm.entity.ProgressBean
 /**
  * @author The one
  * @date 2021-12-10 15:06
- * @describe TODO
+ * @describe 启动界面
  * @email 625805189@qq.com
  * @remark
  */
-class LauncherActivity: BaseCoreActivity<BaseViewModel,ActivityLauncherBinding>(),Animation.AnimationListener{
+class LauncherActivity : BaseCoreActivity<BaseViewModel, ActivityLauncherBinding>() {
 
-    override fun showTopBar(): Boolean = false
-
-    override fun initView(root: View) {
-        loadingView("startAnim")
-        getContentView().postDelayed({
-            QMUIViewHelper.fadeOut(mBinding.loadingView,500,this,true)
-        },2000)
+    companion object {
+        private const val START_ANIM = "startAnim"
+        private const val STOP_ANIM = "startAnim"
     }
 
-    private fun loadingView(action:String){
+    @StringDef(START_ANIM, STOP_ANIM)
+    @kotlin.annotation.Retention(AnnotationRetention.SOURCE)
+    annotation class Action
+
+    /**
+     * 使用反射开启和关闭动画
+     * @param action String
+     */
+    private fun loadingView(@Action action: String) {
         val clazz = WWLoadingView::class.java
         val declaredMethod = clazz.getDeclaredMethod(action)
         declaredMethod.isAccessible = true
         declaredMethod.invoke(mBinding.loadingView)
     }
 
+    override fun showTopBar(): Boolean = false
+
+    override fun initView(root: View) {
+        loadingView(START_ANIM)
+        getContentView().postDelayed({
+            startActivity(MainActivity::class.java, true)
+        }, 2000)
+    }
+
     override fun createObserver() {
 
     }
 
-    override fun onAnimationStart(animation: Animation?) {
-    }
-
-    override fun onAnimationEnd(animation: Animation?) {
-        loadingView("stopAnim")
-        startActivity(MainActivity::class.java,true)
-    }
-
-    override fun onAnimationRepeat(animation: Animation?) {
+    override fun onStop() {
+        loadingView(STOP_ANIM)
+        super.onStop()
     }
 
 }
